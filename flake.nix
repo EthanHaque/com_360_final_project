37034a8028ebc68f5ef3418e58beda0d637bac01
Ethan Haque <ethan.k.haque@gmail.com>
2025-04-23 21:10:36 -0400

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url  = "github:nix-systems/default";
+    devenv.url   = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+
+      packages = forEachSystem (system: {
+        devenv-up   = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          pythonDeps = pkgs.python312.withPackages (ps: with ps; [
+            matplotlib
+            numpy
+            pandas
+          ]);
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                packages = with pkgs; [
+                  pre-commit
+                  pythonDeps
+                ];
+
+                languages.python = {
+                  enable  = true;
+                  version = "3.12.8";
+                  uv.enable = true;
+                  venv = {
+                    enable = true;
+                    quiet  = true;
+                  };
+                };
+              }
+            ];
+          };
+        });
+    };
+}
